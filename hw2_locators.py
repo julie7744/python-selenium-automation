f4955987635865ca579cc001177387667387bba8
Julie Castaneda <juliecastaneda@Julies-iMac.local>
2022-02-20 19:05:05 -0500

    Howework 2 Locators

---
+++ hw2_locators.py
@@ -0,0 +1,17 @@
+Email field = By. ID, "ap_email"
+
+Amazon logo = By. XPATH, "//i[@class='a-icon a-icon-logo']"
+
+Continue button = By. ID, "continue"
+
+Need help link = By. XPATH, "//span[@class='a-expander-prompt']"
+
+Forgot your password link = By. ID, "auth-fpp-link-bottom"
+
+Other issues with Sign-In link = By. ID, "ap-other-signin-issues-link"
+
+Create your Amazon account button = By. ID, "createAccountSubmit"
+
+*Conditions of use link = By. XPATH, "//a[@href='/gp/help/customer/display.html/ref=ap_signin_notification_condition_of_use?ie=UTF8&nodeId=508088']"
+
+*Privacy Notice link = By. XPATH, "//a[@href='/gp/help/customer/display.html/ref=ap_signin_notification_privacy_notice?ie=UTF8&amp;nodeId=468496']"
